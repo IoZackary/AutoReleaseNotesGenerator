b2a1c96e31dfb7db736e8e3aa58b8f3e42eb3a01
Zackary.Breitenstein <Zackary.Breitenstein@evnestnet.com>
2021-04-13 16:28:32 -0700

    POC-2: Add Double Calculator

---
+++ AutoReleaseNotesGenerator/AutoReleaseNotesGenerator.Logic.Tests/DoubleCalcTests/Add.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace AutoReleaseNotesGenerator.Logic.Tests.DoubleCalcTests
+{
+    public class Add
+    {
+        private readonly DoubleCalc doubleCalc;
+
+        public Add()
+        {
+            doubleCalc = new DoubleCalc();
+        }
+        [Theory]
+        [InlineData(1, 3, 4)]
+        [InlineData(52, 46, 98)]
+        [InlineData(19, 5, 24)]
+        [InlineData(16, 16, 32)]
+        [InlineData(1, 70, 71)]
+        public void Test1(int a, int b, int result)
+        {
+            // Arrange
+
+            // Act
+            var actual_result = doubleCalc.Add(a, b);
+
+            // Assert
+            Assert.Equal(result, actual_result);
+        }
+    }
+}
---
+++ AutoReleaseNotesGenerator/AutoReleaseNotesGenerator.Logic/DoubleCalc.cs
@@ -0,0 +1,27 @@
+﻿using AutoReleaseNotesGenerator.Interfaces;
+
+namespace AutoReleaseNotesGenerator.Logic
+{
+    public class DoubleCalc : ICalc<double>
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return b / a;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
