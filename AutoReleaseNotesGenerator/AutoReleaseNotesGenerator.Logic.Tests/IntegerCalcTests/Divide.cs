a1feb670cd88d0958f5ef2d1b4cdc4ab6c9bd515
Zackary.Breitenstein <Zackary.Breitenstein@evnestnet.com>
2021-04-13 15:42:31 -0700

    POC-1: Add IntegerCalc Unit Tests

---
+++ AutoReleaseNotesGenerator/AutoReleaseNotesGenerator.Logic.Tests/IntegerCalcTests/Divide.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutoReleaseNotesGenerator.Logic.Tests.IntegerCalcTests
+{
+    public class Divide
+    {
+        private readonly IntegerCalc integerCalc;
+
+        public Divide()
+        {
+            integerCalc = new IntegerCalc();
+        }
+        [Theory]
+        [InlineData(1, 3, 3)]
+        [InlineData(52, 46, 0)]
+        [InlineData(19, 5, 0)]
+        [InlineData(16, 16, 1)]
+        [InlineData(1, 70, 70)]
+        public void Test1(int a, int b, int result)
+        {
+            // Arrange
+
+            // Act
+            var actual_result = integerCalc.Divide(a, b);
+
+            // Assert
+            Assert.Equal(result, actual_result);
+        }
+    }
+}
---
+++ AutoReleaseNotesGenerator/AutoReleaseNotesGenerator.Logic.Tests/IntegerCalcTests/Subtract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutoReleaseNotesGenerator.Logic.Tests.IntegerCalcTests
+{
+    public class Subtract
+    {
+        private readonly IntegerCalc integerCalc;
+
+        public Subtract()
+        {
+            integerCalc = new IntegerCalc();
+        }
+        [Theory]
+        [InlineData(1, 3, -2)]
+        [InlineData(52, 46, 6)]
+        [InlineData(19, 5, 14)]
+        [InlineData(16, 16, 0)]
+        [InlineData(1, 70, -69)]
+        public void Test1(int a, int b, int result)
+        {
+            // Arrange
+
+            // Act
+            var actual_result = integerCalc.Subtract(a, b);
+
+            // Assert
+            Assert.Equal(result, actual_result);
+        }
+    }
+}
