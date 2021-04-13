399e158636c7f0fcd77da810ac6303d779e857c3
Zackary.Breitenstein <Zackary.Breitenstein@evnestnet.com>
2021-04-13 16:28:32 -0700

    POC-2: Add Double Calculator

---
+++ AutoReleaseNotesGenerator/AutoReleaseNotesGenerator.Logic.Tests/DoubleCalcTests/Divide.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace AutoReleaseNotesGenerator.Logic.Tests.DoubleCalcTests
+{
+
+    public class Divide
+    {
+        private readonly DoubleCalc doubleCalc;
+
+        public Divide()
+        {
+            doubleCalc = new DoubleCalc();
+        }
+        [Theory]
+        [InlineData(7.8, 5.3, 0.67948717948717948717948717948718)]
+        [InlineData(5.2, 4.6, 0.8846153846153845)]
+        [InlineData(1.9, 5, 2.6315789473684211)]
+        [InlineData(1.6, 1.6, 1)]
+        [InlineData(.1, .7, 6.999999999999999)]
+        public void Test1(double a, double b, double result)
+        {
+            // Arrange
+
+            // Act
+            var actual_result = doubleCalc.Divide(a, b);
+
+            // Assert
+            Assert.Equal(result, actual_result);
+        }
+    }
+}
---
+++ AutoReleaseNotesGenerator/AutoReleaseNotesGenerator.Logic.Tests/DoubleCalcTests/Subtract.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace AutoReleaseNotesGenerator.Logic.Tests.DoubleCalcTests
+{
+    public class Subtract
+    {
+        private readonly DoubleCalc doubleCalc;
+
+        public Subtract()
+        {
+            doubleCalc = new DoubleCalc();
+        }
+        [Theory]
+        [InlineData(1, 3, -2)]
+        [InlineData(52, 46, 6)]
+        [InlineData(19, 5, 14)]
+        [InlineData(16, 16, 0)]
+        [InlineData(1, 70, -69)]
+        public void Test1(int a, int b, int result)
+        {
+            // Arrange
+
+            // Act
+            var actual_result = doubleCalc.Subtract(a, b);
+
+            // Assert
+            Assert.Equal(result, actual_result);
+        }
+    }
+}
